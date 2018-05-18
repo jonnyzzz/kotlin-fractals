d9fc5c64630c9088a30e3727150a582c617d6c7d
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-19 01:18:41 +0200

    setup ktor for the server

--- jvm/src/main/kotlin/main.kt
+++ jvm/src/main/kotlin/main.kt
@@ -1,7 +1,29 @@
 package org.jetbrains.demo.kotlinfractals
 
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 
 fun main(args: Array<String>) {
+  val server = embeddedServer(Netty, 8080, module = Application::main)
+  server.start(wait = true)
+}
 
+fun Application.main() {
+  install(DefaultHeaders)
+  install(CallLogging)
+  install(Routing) {
+    get("/") {
+      call.respondText("Hello, World!")
+    }
+  }
 }
 
