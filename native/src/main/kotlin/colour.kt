d89ae68ca63e2db3430533a8afcd077eb0ded9c4
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-21 23:24:21 +0200

    ++

--- native/src/main/kotlin/colour.kt
+++ native/src/main/kotlin/colour.kt
@@ -2,7 +2,10 @@ package org.jetbrains.demo.kotlinfractals
 
 import org.jetbrains.demo.kotlinfractals.Color
 
-actual fun colorFromHSL(h : Double, s: Double, l: Double) : Color {
+actual fun colorFromHSL(h: Double,
+                        s: Double,
+                        l: Double) : Color {
   //OK for console mode, for now
   return Color.WHITE
 }
+
