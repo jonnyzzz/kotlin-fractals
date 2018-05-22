9cdb24824cbe93a6c469d2c95f166ac4359facd7
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-22 12:49:14 +0200

    format code

--- jvm/src/main/kotlin/colour.kt
+++ jvm/src/main/kotlin/colour.kt
@@ -3,8 +3,10 @@ package org.jetbrains.demo.kotlinfractals
 import www.camick.com.HSLColor
 
 
-actual fun colorFromHSL(h : Double, s: Double, l: Double) : Color {
-  val x = HSLColor(h.toFloat(),s.toFloat(),l.toFloat()).rgb
+actual fun colorFromHSL(h: Double,
+                        s: Double,
+                        l: Double): Color {
+  val x = HSLColor(h.toFloat(), s.toFloat(), l.toFloat()).rgb
   return Color(x.red, x.green, x.blue)
 }
 
