7ea4384ca85182a68797f3b2ad2c1a937eeb0d4c
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-24 00:11:24 +0200

    minor

--- common/src/main/kotlin/render.kt
+++ common/src/main/kotlin/render.kt
@@ -1,18 +1,17 @@
 package org.jetbrains.demo.kotlinfractals
 
 interface FractalImage {
-  val pixelRect : Rect<Int>
+  val pixelRect: Rect<Int>
   fun putPixel(p: Pixel, c: Color)
 }
 
 object MandelbrotRender {
   val initialArea = Rect(-2.0, -2.0, 2.0, 2.0)
 
-  fun justRender(maxIterations : Int = 1500,
+  fun justRender(maxIterations: Int = 1500,
                  image: FractalImage,
                  area: Rect<Double>) {
-
-    val  t = Transformation(image.pixelRect, area)
+    val t = Transformation(image.pixelRect, area)
 
     val picker = ColorPicker(maxIterations)
 
