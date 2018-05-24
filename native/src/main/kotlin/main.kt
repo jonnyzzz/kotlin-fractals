f876beff62a841ff3f8dcc1004030e89e047727f
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-24 15:00:06 +0200

    parse parameters

--- native/src/main/kotlin/main.kt
+++ native/src/main/kotlin/main.kt
@@ -1,10 +1,19 @@
 import org.jetbrains.demo.kotlinfractals.*
 
+fun Array<String>.arg(arg: String, def: Int) : Int  {
+  val prefix = "--$arg="
+  return firstOrNull { it.startsWith(prefix) }?.
+          removePrefix(prefix)?.
+          toInt() ?: def
+}
 
 fun main(args: Array<String>) {
   println("Kotlin Fractals Native!")
 
-  val image = NativeImage(80, 40)
+  val width = args.arg("width", 80)
+  val height = args.arg("height", 40)
+
+  val image = NativeImage(width, height)
 
   MandelbrotRender.justRender(
           image = image,
