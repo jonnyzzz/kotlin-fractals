699a35ec3a1fb81b998172f57a40062d71ec67e7
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-24 14:37:25 +0200

    format code

--- common/src/main/kotlin/colour.kt
+++ common/src/main/kotlin/colour.kt
@@ -2,17 +2,17 @@ package org.jetbrains.demo.kotlinfractals
 
 import kotlin.math.ln
 
-      // common code
-      expect class Color
+// common code
+expect class Color
 
-      object Colors
+object Colors
 
-      expect fun Colors.hsl(h: Double,
-                           s: Double,
-                           l: Double) : Color
+expect fun Colors.hsl(h: Double,
+                      s: Double,
+                      l: Double): Color
 
-      expect val Colors.BLACK: Color
-      expect val Colors.WHITE: Color
+expect val Colors.BLACK: Color
+expect val Colors.WHITE: Color
 
 
 class ColorPicker(
@@ -23,7 +23,7 @@ class ColorPicker(
       return Colors.BLACK
     }
 
-    val s = z.iteration + 1 - ln( ln(z.mod2) / 2.0) / ln(2.0)
+    val s = z.iteration + 1 - ln(ln(z.mod2) / 2.0) / ln(2.0)
     return Colors.hsl(30.0 + 10 * s, 90.0, 50.0)
   }
 }
