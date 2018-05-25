3eb55c7b05660c0fcd184dacfae1d9679c4c308b
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-05-25 11:26:36 +0200

    monor

--- js/src/main/kotlin/colour.kt
+++ js/src/main/kotlin/colour.kt
@@ -6,12 +6,11 @@ import kotlin.math.absoluteValue
 private val lib = require("color-convert")
 
 actual fun Colors.hsl(h: Double,
-                      s: Double,
-                      l: Double): Color {
-  val x = lib.hsl.rgb(h, s, l)
-  fun N(i : Int)
-          = (x[i] as Int).absoluteValue % 256
-  return Color(N(0), N(1), N(2))
+                     s: Double,
+                     l: Double): Color {
+ val x = lib.hsl.rgb(h, s, l)
+ fun c(i : Int) = (x[i] as Int).absoluteValue % 256
+ return Color(c(0), c(1), c(2))
 }
 
 actual class Color(val r: Int, val g: Int, val b: Int)
